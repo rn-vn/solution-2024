25f8781c12a195dddeb2002ffc11b5561320b04b
EC2 Default User <ec2-user@ip-172-31-75-112.ec2.internal>
2023-12-16 13:30:25 +0000

    login with firebase

---
+++ src/FirebaseConfig.js
@@ -0,0 +1,25 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// import { getAnalytics } from "firebase/analytics";
+
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBDanlkMV3YOxYKaM0WiMvMQJ16Jnug72g",
+  authDomain: "solution-2024.firebaseapp.com",
+  projectId: "solution-2024",
+  storageBucket: "solution-2024.appspot.com",
+  messagingSenderId: "249284415584",
+  appId: "1:249284415584:web:bc5da1f9b955b9c5d07305",
+  measurementId: "G-QN04LNLTK9"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+// const analytics = getAnalytics(app);
+
+export const auth = getAuth(app);
---
+++ src/Mypage.js
@@ -0,0 +1,53 @@
+import React, { useState, useEffect } from "react";
+/* 「onAuthStateChanged」と「auth」をimport↓ */
+import { onAuthStateChanged, signOut } from "firebase/auth";
+import { auth } from "./FirebaseConfig.js";
+/* ↓「Navigate」をimport */
+import {
+  useNavigate,
+  Navigate
+} from "react-router-dom";
+
+const Mypage = () => {
+  /* ↓state変数「user」を定義 */
+  const [user, setUser] = useState("");
+  const [loading, setLoading] = useState(true);
+
+  /* ↓ログインしているかどうかを判定する */
+  useEffect(() => {
+    onAuthStateChanged(auth, (currentUser) => {
+      setUser(currentUser);
+      setLoading(false);
+    });
+  }, []);
+  
+  /* ↓「navigate」を定義 */
+  const navigate = useNavigate();
+  
+  /* ↓関数「logout」を定義 */
+  const logout = async () => {
+    await signOut(auth);
+    navigate("/login/");
+  }
+
+  return (
+    <>
+      {/* ↓「loading」がfalseのときにマイページを表示する設定 */}
+      {!loading && (
+        <>
+          {!user ? (
+            <Navigate to={`/login/`} />
+          ) : (
+            <>
+              <h1>マイページ</h1>
+              <p>{user?.email}</p>
+              <button onClick={logout}>ログアウト</button>
+            </>
+          )}
+        </>
+      )}
+    </>
+  );
+};
+
+export default Mypage;
