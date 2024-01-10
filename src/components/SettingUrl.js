c2e7135034e97fcb01260ef42a0bd3c9ca3b0a47
rn-vn <taka0102rin@gmail.com>
2024-01-10 18:12:22 +0900

    feat: add url

---
+++ src/components/SettingUrl.js
@@ -0,0 +1,23 @@
+// いい教材あればURL追加していってください！
+
+const SettingUrl = [
+  'https://sdgs.edutown.jp/info/goals/goals-1.html',
+  'https://sdgs.edutown.jp/info/goals/goals-2.html',
+  'https://sdgs.edutown.jp/info/goals/goals-3.html',
+  'https://sdgs.edutown.jp/info/goals/goals-4.html',
+  'https://sdgs.edutown.jp/info/goals/goals-5.html',
+  'https://sdgs.edutown.jp/info/goals/goals-6.html',
+  'https://sdgs.edutown.jp/info/goals/goals-7.html',
+  'https://sdgs.edutown.jp/info/goals/goals-8.html',
+  'https://sdgs.edutown.jp/info/goals/goals-9.html',
+  'https://sdgs.edutown.jp/info/goals/goals-10.html',
+  'https://sdgs.edutown.jp/info/goals/goals-11.html',
+  'https://sdgs.edutown.jp/info/goals/goals-12.html',
+  'https://sdgs.edutown.jp/info/goals/goals-13.html',
+  'https://sdgs.edutown.jp/info/goals/goals-14.html',
+  'https://sdgs.edutown.jp/info/goals/goals-15.html',
+  'https://sdgs.edutown.jp/info/goals/goals-16.html',
+  'https://sdgs.edutown.jp/info/goals/goals-17.html',
+];
+
+export default SettingUrl;
