09c77dfb1202fb8ec9e2a142cc6adcb59ea1faa0
EC2 Default User <ec2-user@ip-172-31-75-112.ec2.internal>
2024-02-12 08:46:14 +0000

    ビンゴにルート追加

--- src/components/Bingo.js
+++ src/components/Bingo.js
@@ -19,21 +19,35 @@ const Bingo = () => {
           <img  className='bingotitle' src={bingotitle} alt="bingotitle" width={160} height={69}/>
         </div>
         <div className='bingo-firststage'>
-          <a href="/bingo1/">
+          <a href="/bingo/1">
             <img className='bingo1' src={bingo1} alt="bingo1" width={87} height={87}/>
-          </a>  
+          </a>
+          <a href="/bingo/2">
             <img className='bingo2' src={bingo2} alt="bingo2" width={87} height={87}/>
+          </a>  
+          <a href="/bingo/3">  
             <img className='bingo3' src={bingo3} alt="bingo3" width={87} height={87}/>
+          </a>  
         </div>
         <div className='bingo-secondstage'>
-          <img className='bingo4' src={bingo4} alt="bingo4" width={87} height={87}/>
-          <img className='bingostar' src={Star} alt="Star" width={87} height={87}/>
-          <img className='bingo6' src={bingo6} alt="bingo6" width={87} height={87}/>
+          <a href="/bingo/4">
+            <img className='bingo4' src={bingo4} alt="bingo4" width={87} height={87}/>
+          </a>  
+            <img className='bingostar' src={Star} alt="Star" width={87} height={87}/>
+          <a href="/bingo/6">  
+            <img className='bingo6' src={bingo6} alt="bingo6" width={87} height={87}/>
+          </a>  
         </div>
         <div className='bingo-thirdstage'>
-          <img className='bingo7' src={bingo7} alt="bingo7" width={87} height={87}/>
-          <img className='bingo8' src={bingo8} alt="bingo8" width={87} height={87}/>
-          <img className='bingo9' src={bingo9} alt="bingo9" width={87} height={87}/>
+          <a href="/bingo/7">        
+            <img className='bingo7' src={bingo7} alt="bingo7" width={87} height={87}/>
+          </a>  
+          <a href="/bingo/8">  
+            <img className='bingo8' src={bingo8} alt="bingo8" width={87} height={87}/>
+          </a>  
+          <a href="/bingo/9">  
+            <img className='bingo9' src={bingo9} alt="bingo9" width={87} height={87}/>
+          </a>  
         </div>
       </div>
     </div>
