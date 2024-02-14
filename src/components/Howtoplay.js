6cdc7b3fa81e4721bc53c729d4ac1ba49d8d0e71
Riko <a23.mxxw@g.chuo-u.ac.jp>
2024-02-14 12:34:44 +0900

    how to play

--- src/components/Howtoplay.js
+++ src/components/Howtoplay.js
@@ -4,7 +4,61 @@ import HomeFooter from './HomeFooter'
 const Howtoplay = () => {
   return (
     <>
-      <p>Howtoplay</p>
+      <h1>About SDGs bingo</h1>
+      <p>
+        SDGs Bingo is a fun game to learn about actions to achieve the SDGs goals in a bingo format!
+        Launch the app and display the bingo card on the home screen.
+        Tap the numbers displayed on the screen to complete the given mission!
+        The missions are to take actions related to the SDGs goals and to acquire knowledge about the SDGs.
+        When you get three "Clear" cards vertically, horizontally, or diagonally, you have a Bingo!
+        Bingo cards will be updated in a week.
+      </p>
+
+      <h1>How to play</h1>
+
+      <p>
+        1.Launch the application.<br/>
+        2.Display the bingo card on the home screen.<br/>
+        3.Tap the number displayed on the screen.<br/>
+        4.Clear the mission.<br/>
+        5.Clear a number on the bingo card.<br/>
+      </p>
+
+      <h1>Mission</h1>
+
+      <p>To take actions related to the SDGs goals and to acquire knowledge about the SDGs.</p>
+
+      <p>
+        For example <br/>
+         "Throw away plastic bottles"<br/>
+         "Use eco bags"<br/>
+         "Read articles about the SDGs"<br/>
+         "Find out about the SDGs goals"<br/>
+         and other missions are available.<br/>
+      </p>
+
+      <h1>Bingo cards</h1>
+
+      <p>
+        Bingo cards are assigned a number from 1 to 9 for each SDG goal.
+        When you complete a mission, the assigned number will be cleared.
+        When three Clear cards are aligned vertically, horizontally, or diagonally, you have a Bingo!
+      </p>
+
+      <h1>Bingo Card Update</h1>
+
+      <p>
+        Bingo cards are updated every week.
+        New bingo cards will have new missions.
+      </p>
+
+      <h1>Learn about the SDGs</h1>
+
+      <p>
+        Read articles related to the SDGs
+        You can clear one of your bingo cards if you write a summary of at least a specified number of words.
+        The more you learn about the SDGs, the easier it will be to clear your bingo card!
+      </p>
       <HomeFooter />
     </>
   )
